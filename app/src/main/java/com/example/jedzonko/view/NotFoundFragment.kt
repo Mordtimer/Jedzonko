4ebe078c2694aa30c23b216946be7a468c0ff325
Nardox121 <uminski.marcin1@gmail.com>
2021-02-06 18:04:37 +0100

    fragment classes + navigation actions

---
+++ app/src/main/java/com/example/jedzonko/view/NotFoundFragment.kt
@@ -0,0 +1,7 @@
+package com.example.jedzonko.view
+
+import androidx.fragment.app.Fragment
+import com.example.jedzonko.R
+
+class NotFoundFragment : Fragment(R.layout.not_found_fragment) {
+}
---
+++ app/src/main/java/com/example/jedzonko/view/SummaryFragmen.kt
@@ -0,0 +1,7 @@
+package com.example.jedzonko.view
+
+import androidx.fragment.app.Fragment
+import com.example.jedzonko.R
+
+class SummaryFragment : Fragment(R.layout.summary_fragment) {
+}
