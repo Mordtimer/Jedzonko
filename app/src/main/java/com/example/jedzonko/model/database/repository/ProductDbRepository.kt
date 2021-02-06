b62abb68d67be915f39563a70639cc18b8c39ff7
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-02-06 22:04:13 +0100

    Changed ProductDbRepository.kt name

--- app/src/main/java/com/example/jedzonko/model/database/repository/ProductDbRepository.kt
+++ app/src/main/java/com/example/jedzonko/model/database/repository/ProductDbRepository.kt
@@ -6,7 +6,7 @@ import com.example.jedzonko.model.database.NutrimentDB
 import com.example.jedzonko.model.database.ProductDB
 import com.example.jedzonko.model.database.ProductDao
 
-class ProductDBRepository(private val productDao: ProductDao) {
+class ProductDbRepository(private val productDao: ProductDao) {
     val readAll: LiveData<List<ProductDB>> = productDao.all()
 
     suspend fun add(product: ProductDB, nutriment: NutrimentDB) {
--- app/src/main/java/com/example/jedzonko/viewModel/ProductVM.kt
+++ app/src/main/java/com/example/jedzonko/viewModel/ProductVM.kt
@@ -2,18 +2,15 @@ package com.example.jedzonko.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.jedzonko.model.Product
 import com.example.jedzonko.model.ProductRepository
 import com.example.jedzonko.model.Request
 import com.example.jedzonko.model.database.MyDatabase
 import com.example.jedzonko.model.database.NutrimentDB
 import com.example.jedzonko.model.database.ProductDB
-import com.example.jedzonko.model.database.repository.ProductDBRepository
+import com.example.jedzonko.model.database.repository.ProductDbRepository
 import kotlinx.coroutines.launch
-import retrofit2.Call
 import retrofit2.Response
 import java.util.*
 
@@ -22,8 +19,8 @@ class ProductVM(application: Application, private val repository: ProductReposit
     val productResponse: MutableLiveData<Response<Request>> = MutableLiveData()
     var productCode:String = ""
 
-    private val productRepository: ProductDBRepository =
-            ProductDBRepository(MyDatabase.getDatabase(application).productDao())
+    private val productRepository: ProductDbRepository =
+            ProductDbRepository(MyDatabase.getDatabase(application).productDao())
 
     fun addProduct(){
         if(productResponse.value?.body() != null) {
