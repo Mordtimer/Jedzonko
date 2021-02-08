57ad035b6e559441f991a4d181887d1919346d89
Mordtimer <filipgab99@gmail.com>
2021-02-08 10:30:30 +0100

    fixing merge conflict

--- app/src/main/java/com/example/jedzonko/view/DetailsFragment.kt
+++ app/src/main/java/com/example/jedzonko/view/DetailsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
